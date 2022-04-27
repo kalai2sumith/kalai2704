c470484cc4b2a00e0197dda1684d5afaa1819b8a
kalai2sumith <kalaiskky92@gmail.com>
2022-04-27 11:34:52 +0530

    kalai2704

---
+++ src/org/emp/Employee.java
@@ -0,0 +1,55 @@
+package org.emp;
+
+public class Employee { 
+private void empId(){
+	System.out.println("empId is 4321");	
+
+}
+private void empName() {
+
+System.out.println("empName is kalai"); 
+}
+private void empDob() {
+	System.out.println("empDOb is 1992");
+
+}
+private void empPhone() {
+	System.out.println("empPhone no is 766276272727");
+
+}
+
+public static void main(String[] args) {
+//className objectName=className
+Employee ee=new Employee();
+ee.empId();
+ee.empDob();
+ee.empName();
+ee.empPhone();
+
+
+
+
+
+
+
+
+}	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
